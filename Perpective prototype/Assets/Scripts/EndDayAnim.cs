13280e200b27c6936a5d0330788fd84abd7a1850
Fowlerpro <jaycefowler@outlook.com>
2026-03-25 15:04:32 -0400

    add Month Text Updates

--- Perpective prototype/Assets/Scripts/EndDayAnim.cs
+++ Perpective prototype/Assets/Scripts/EndDayAnim.cs
@@ -1,15 +1,24 @@
+using TMPro;
 using UnityEngine;
 
 public class EndDayAnim : MonoBehaviour
 {
+    public TextMeshProUGUI Text;
     private Animator PrinterAnim;
+    private int month = 1;
 
     void Start()
     {
         PrinterAnim = GetComponent<Animator>();
     }
+    private void Update()
+    {
+        Text.text = "Month " + month.ToString();
+    }
     public void OnButtonClick()
     {
         PrinterAnim.SetTrigger("EPress");
+        month ++;
     }
+
 }
